4565ef58f9fda2c1fda2cbad56c8f1925ae9fbb2
josielynngriffin <jelynngriffin@gmail.com>
2023-08-01 10:56:46 -0400

    Add styling to regular navigation, modify classes as needed

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,17 +3,17 @@ import React from 'react';
 function Header(props) {
     const {onAbout, onPortfolio, onContact, onLanding, onResume} = props;
     return (
-        <div className= "Header-page">
+        <div className= "header-wrap">
             <header>
-                <h1 classname="portfolio-name"><a onClick={onLanding}>josie griffin</a></h1>
+                <h1 className="portfolio-name"><a onClick={onLanding}>josie griffin</a></h1>
             </header>
-            <nav className='portfolio-nav'>
-                <ul>
-                    <li><a onClick={onAbout}>about me</a></li>
-                    <li><a onClick={onPortfolio}>portfolio</a></li>
-                    <li><a onClick={onContact}>contact</a></li>
-                    <li><a onClick={onResume}>resume</a></li>
-                    <li><i className="fa-solid fa-circle-half-stroke"></i></li>
+            <nav>
+                <ul className='portfolio-nav'>
+                    <li className='portfolio__nav-item'><a onClick={onAbout}>about me</a></li>
+                    <li className='portfolio__nav-item'><a onClick={onPortfolio}>portfolio</a></li>
+                    <li className='portfolio__nav-item'><a onClick={onContact}>contact</a></li>
+                    <li className='portfolio__nav-item portfolio__nav-button'><a onClick={onResume}>resume</a></li>
+                    <li className='portfolio__nav-item'><i className="portfolio__nav-icon fa-solid fa-circle-half-stroke"></i></li>
                 </ul>
             </nav>
         </div>
