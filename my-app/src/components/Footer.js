128f0da385ae57e604f83ab9d157ac90b5812aed
josielynngriffin <jelynngriffin@gmail.com>
2023-08-01 12:37:01 -0400

    Add styling to landing, footer, and modify classes in components as needed

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer>
-            <h4>josie griffin</h4>
-            <hr></hr>
-            <small>jelynngriffin@gmail.com | © josie griffin dev</small>
+        <footer classname="footer__wrapper">
+            <h4 className="footer__name">josie griffin</h4>
+            <hr className="footer__break"></hr>
+            <small className="footer__small">jelynngriffin@gmail.com | © josie griffin dev</small>
         </footer>
     );
 }
--- my-app/src/components/Landing.js
+++ my-app/src/components/Landing.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import headshot from '../assets/images/darkmode.png';
+import headshot from '../assets/images/lightmode.png';
 
 function Landing() {
     //console.log(props);
     return (
-        <div className="landing-page">
-            <h1>hi, i'm josie!</h1>
-            <h2> &gt; aspiring full stack web developer</h2>
-            <h2> &gt; based in pennsylvania</h2>
-            <p>looking to combine creativity, user-friendly and inclusive designs, and function!</p> 
-            <p>i believe in working together toward something greater and making an impact. welcome to my portfolio!</p>
+        <div className="landing__wrapper">
+            <section className="landing__text">
+                <h1 className="landing__title">hi, i'm <span className="landing__name--color">josie</span>!</h1>
+                <h2 className="landing__subtitle"> &gt; aspiring <span className="landing__job-title--color">full stack web developer</span></h2>
+                <h2 className="landing__subtitle"> &gt; based in pennsylvania</h2>
+                <p className="landing__blurb">looking to combine creativity, user-friendly and inclusive designs, and function!</p> 
+                
+                <p className="landing__blurb">i believe in working together toward something greater and making an impact. welcome to my portfolio!</p>
+            </section>
             <figure>
-                <img className='landing-headshot' src={headshot} alt='Headshot of Josie Griffin in black and white'/>
+                <img className='landing-headshot' src={headshot} alt='Headshot of Josie Griffin from her shoulders up in black and white'/>
             </figure>
         </div>
     );
