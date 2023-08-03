1dd5144b869c98a5d1d7626d98cc89b1d411ce92
josielynngriffin <jelynngriffin@gmail.com>
2023-08-02 22:36:57 -0400

    Add redirect event and styling to footer name to return to landing on click

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-function Footer() {
+function Footer(props) {
+    const {onLanding} = props;
     return (
         <footer classname="footer__wrapper">
-            <h4 className="footer__name">josie griffin</h4>
+            <h4 className="footer__name" onClick={onLanding}>josie griffin</h4>
             <hr className="footer__break"></hr>
             <small className="footer__small">jelynngriffin@gmail.com | © josie griffin dev</small>
-            <section className="footer-contact">
+            <section className="footer__contact">
                 <a href="https://github.com/josielynngriffin" target="_blank"><i className="portfolio__icon fa-brands fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/josiegriffin/" target="_blank"><i className="portfolio__icon fa-brands fa-linkedin"></i></a>
                 <a href="mailto: jelynngriffin@gmail.com" target="_blank"><i className="portfolio__icon fa-solid fa-envelope"></i></a>
