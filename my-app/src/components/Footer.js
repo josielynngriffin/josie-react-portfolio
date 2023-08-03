9747703d628c12ff8d50954a8800f758a77f2778
josielynngriffin <jelynngriffin@gmail.com>
2023-08-02 22:31:31 -0400

    Add contact information to footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -6,6 +6,11 @@ function Footer() {
             <h4 className="footer__name">josie griffin</h4>
             <hr className="footer__break"></hr>
             <small className="footer__small">jelynngriffin@gmail.com | © josie griffin dev</small>
+            <section className="footer-contact">
+                <a href="https://github.com/josielynngriffin" target="_blank"><i className="portfolio__icon fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/josiegriffin/" target="_blank"><i className="portfolio__icon fa-brands fa-linkedin"></i></a>
+                <a href="mailto: jelynngriffin@gmail.com" target="_blank"><i className="portfolio__icon fa-solid fa-envelope"></i></a>
+            </section>
         </footer>
     );
 }
