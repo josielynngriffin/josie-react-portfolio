629f02ce5141b5444c88884cb65340105c026a28
josielynngriffin <jelynngriffin@gmail.com>
2023-08-02 21:29:09 -0400

    Fix typo

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -4,7 +4,7 @@ import pdf from '../assets/images/josie-griffin-resume.pdf';
 function Resume() {
     return (
         <div className="resume__page">
-            <h1 className="resume__title"><span className="title__break">|</span> my portfolio</h1>
+            <h1 className="resume__title"><span className="title__break">|</span> my resume</h1>
             <div className="pdf__wrap">
                 <embed src={pdf} type="application/pdf" width="100%" height="600px" />
             </div>
