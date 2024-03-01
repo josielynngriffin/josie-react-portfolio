8b0e42b3c9d2e7a2d42586833faa6f4e764472ce
josielynngriffin <jelynngriffin@gmail.com>
2024-02-29 21:03:32 -0500

    Modify text content

--- my-app/src/pages/Landing.js
+++ my-app/src/pages/Landing.js
@@ -7,7 +7,7 @@ function Landing() {
         <div className="landing__wrapper">
             <section className="landing__text">
                 <h1 className="landing__title">hi, i'm <span className="landing__name--color">josie</span>!</h1>
-                <h2 className="landing__subtitle"> &gt; aspiring <span className="landing__job-title--color">full stack web developer</span></h2>
+                <h2 className="landing__subtitle"> &gt; <span className="landing__job-title--color">full stack web developer</span></h2>
                 <h2 className="landing__subtitle">&gt; ux/ui <span className="landing__job-title--color">researcher</span> & <span className="landing__job-title--color">designer</span> </h2>
                 <h2 className="landing__subtitle"> &gt; based in pennsylvania</h2>
                 <p className="landing__blurb">I’m on a quest to combine creativity, user-friendly design, and inclusivity into adventurous digital experiences!</p> 
