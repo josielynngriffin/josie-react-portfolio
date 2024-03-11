7ede48e7438e5a67327647180b80f9bdafd1e495
josielynngriffin <jelynngriffin@gmail.com>
2024-03-11 17:24:53 -0400

    Add content mockup for left and right case study components

--- my-app/src/components/LeftCS.js
+++ my-app/src/components/LeftCS.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-function LeftCS() {
+function LeftCS({header, subtitle, textContent, imgLabel, img, imgAlt, imgDescription}) {
+    const paragraphs = textContent.split('\n');
+
     return (
-        <div className="leftCS__wrapper">
-            
+        <div className="projectIntroCS__wrapper leftCS__wrapper case-study__component--margin">
+            <section className="projectIntro--text-wrap leftCS__wrapper">
+                <h3>{header}</h3>
+                <h4>{subtitle}</h4>
+                {paragraphs.map((paragraph, index) => (
+                    <p key={index} className="case-study-text">{paragraph}</p>
+                ))}
+            </section>
+            <section className="projectIntro--image-wrap">
+                <p className="label-text">{imgLabel}</p>
+                <img  className="projectIntro--image" src={img} alt={imgAlt}></img>
+                <p className="small-text imgDescription">{imgDescription}</p>
+            </section>
         </div>
     );
 }
--- my-app/src/components/RightCS.js
+++ my-app/src/components/RightCS.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-function RightCS() {
+function RightCS({header, subtitle, textContent, imgLabel, img, imgAlt, imgDescription}) {
+    const paragraphs = textContent.split('\n');
+
     return (
-        <div className="rightCS__wrapper">
-            
+        <div className="projectIntroCS__wrapper rightCS__wrapper case-study__component--margin">
+           <section className="projectIntro--text-wrap rightCS__wrapper">
+                <h3>{header}</h3>
+                <h4>{subtitle}</h4>
+                {paragraphs.map((paragraph, index) => (
+                    <p key={index} className="case-study-text">{paragraph}</p>
+                ))}
+            </section>
+            <section className="projectIntro--image-wrap">
+                <p className="label-text">{imgLabel}</p>
+                <img  className="projectIntro--image" src={img} alt={imgAlt}></img>
+                <p className="small-text imgDescription">{imgDescription}</p>
+            </section> 
         </div>
     );
 }
