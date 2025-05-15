2cfc8c8fdedf54450eb88c80ca84ccef7b874d65
josielynngriffin <jelynngriffin@gmail.com>
2025-05-15 10:02:39 -0400

    Add code for full-width case study component and modify project intro

--- my-app/src/components/FullCS.js
+++ my-app/src/components/FullCS.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-function FullCS() {
+function FullCS({header, subtitle, textContent, imgLabel, img, imgAlt, imgDescription}) {
+    const paragraphs = textContent.split('\n');
+
     return (
-        <div className="fullCS__wrapper">
-            
+        <div className="projectIntroCS__wrapper fullCS__wrapper case-study__component--margin">
+            <section className="fullCS--text-wrap fullCS__wrapper">
+                <h3>{header}</h3>
+                <h4>{subtitle}</h4>
+                {paragraphs.map((paragraph, index) => (
+                    <p key={index} className="case-study-text">{paragraph}</p>
+                ))}
+            </section>
+            <section className="fullCS--image-wrap">
+                <p className="label-text">{imgLabel}</p>
+                <img  className="fullCS--image" src={img} alt={imgAlt}></img>
+                <p className="small-text imgDescription">{imgDescription}</p>
+            </section>
         </div>
     );
 }
