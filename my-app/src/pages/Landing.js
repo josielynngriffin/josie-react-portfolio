0fb43540b95fc8ce3f97e63ab7409a79d53ab84a
josielynngriffin <jelynngriffin@gmail.com>
2025-05-19 10:28:58 -0400

    Swap job titles

--- my-app/src/pages/Landing.js
+++ my-app/src/pages/Landing.js
@@ -7,14 +7,14 @@ function Landing() {
         <div className="landing__wrapper">
             <section className="landing__text">
                 <h1 className="landing__title">hi, i'm <span className="landing__name--color">josie</span>!</h1>
-                <h2 className="landing__subtitle"> &gt; <span className="landing__job-title--color">full stack web developer</span></h2>
                 <h2 className="landing__subtitle">&gt; ux/ui <span className="landing__job-title--color">researcher</span> & <span className="landing__job-title--color">designer</span> </h2>
+                <h2 className="landing__subtitle"> &gt; <span className="landing__job-title--color">full stack web developer</span></h2>
                 <h2 className="landing__subtitle"> &gt; based in pennsylvania</h2>
                 <p className="landing__blurb">I’m on a quest to combine creativity, user-friendly design, and inclusivity into adventurous digital experiences!</p> 
                 
                 <p className="landing__blurb">I firmly believe in collaborative efforts to not only find a solution, but to achieve something greater together and make a lasting impact. Welcome to my portfolio - let’s build something together!</p>
             </section>
-            <figure>
+            <figure className="landing__image-wrap">
                 <img className='landing-headshot' src={headshot} alt='Headshot of Josie Griffin from her shoulders up in black and white'/>
             </figure>
         </div>
