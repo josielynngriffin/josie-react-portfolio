2bbfbe69d7888b585dce4245b329511b1f03fae8
josielynngriffin <jelynngriffin@gmail.com>
2025-05-19 10:34:48 -0400

    Make class updates to case study components and add in other functionality

--- my-app/src/components/FullCS.js
+++ my-app/src/components/FullCS.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
-function FullCS({header, subtitle, textContent, imgLabel, img, imgAlt, imgDescription}) {
+function FullCS({header, subtitle, textContent, listContent, imgLabel, img, imgAlt, imgDescription, className = ""}) {
     const paragraphs = textContent.split('\n');
+    const bullets= listContent ? listContent.split('\n').filter(b => b.trim() !== '') : [];
 
     return (
-        <div className="projectIntroCS__wrapper fullCS__wrapper case-study__component--margin">
-            <section className="fullCS--text-wrap fullCS__wrapper">
+        <div className={`CS-component__wrapper case-study__padding fullCS__wrapper ${className}`}>
+            <section className="fullCS--text-wrap">
                 <h3>{header}</h3>
                 <h4>{subtitle}</h4>
                 {paragraphs.map((paragraph, index) => (
                     <p key={index} className="case-study-text">{paragraph}</p>
                 ))}
+                <ul className="CS-component__list">{bullets.map((bullet, index) => (
+                    <li key={index} className="case-study-text">{bullet}</li>
+                ))}</ul>
             </section>
             <section className="fullCS--image-wrap">
                 <p className="label-text">{imgLabel}</p>
-                <img  className="fullCS--image" src={img} alt={imgAlt}></img>
+                <img  className="fullCS--image case-study-image__radius" src={img} alt={imgAlt}></img>
                 <p className="small-text imgDescription">{imgDescription}</p>
             </section>
         </div>
--- my-app/src/components/LeftCS.js
+++ my-app/src/components/LeftCS.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
-function LeftCS({header, subtitle, textContent, imgLabel, img, imgAlt, imgDescription}) {
+function LeftCS({header, subtitle, textContent, listContent, imgLabel, img, imgAlt, imgDescription, className = ""}) {
     const paragraphs = textContent.split('\n');
+    const bullets= listContent ? listContent.split('\n').filter(b => b.trim() !== '') : [];
 
     return (
-        <div className="projectIntroCS__wrapper leftCS__wrapper case-study__component--margin">
+        <div className={`CS-component__wrapper leftCS__wrapper case-study__padding ${className}`}>
             <section className="projectIntro--text-wrap leftCS__wrapper">
                 <h3>{header}</h3>
                 <h4>{subtitle}</h4>
                 {paragraphs.map((paragraph, index) => (
                     <p key={index} className="case-study-text">{paragraph}</p>
                 ))}
+                <ul className="CS-component__list">{bullets.map((bullet, index) => (
+                    <li key={index} className="case-study-text">{bullet}</li>
+                ))}</ul>
             </section>
-            <section className="projectIntro--image-wrap">
+            <section className="sideCS--image-wrap">
                 <p className="label-text">{imgLabel}</p>
-                <img  className="projectIntro--image" src={img} alt={imgAlt}></img>
+                <img  className="sideCS--image case-study-image__radius" src={img} alt={imgAlt}></img>
                 <p className="small-text imgDescription">{imgDescription}</p>
             </section>
         </div>
