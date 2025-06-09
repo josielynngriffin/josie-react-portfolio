e1e2b228fe8184a54f6364c24e8374cd969abd0c
josielynngriffin <jelynngriffin@gmail.com>
2025-06-09 15:27:25 -0400

    Add featured projects highlight and fix button styles

--- my-app/src/pages/Landing.js
+++ my-app/src/pages/Landing.js
@@ -2,26 +2,57 @@ import React from 'react';
 import headshot from '../assets/images/lightmode-headshot.png';
 import {Link} from 'react-router-dom';
 
-function Landing() {
+function Landing({uxProjects}) {
     //console.log(props);
     return (
-        <div className="landing__wrapper">
-            <section className="landing__text">
-                <h1 className="landing__title">hi, i'm <span className="landing__name--color">josie</span>!</h1>
-                <h2 className="landing__subtitle">&gt; ux/ui <span className="landing__job-title--color">researcher</span> & <span className="landing__job-title--color">designer</span> </h2>
-                <h2 className="landing__subtitle"> &gt; <span className="landing__job-title--color">full stack web developer</span></h2>
-                <h2 className="landing__subtitle"> &gt; based in pennsylvania</h2>
-                <p className="landing__blurb">I’m on a quest to combine creativity, user-friendly design, and inclusivity into adventurous digital experiences!</p> 
-                <p className="landing__blurb">I firmly believe in collaborative efforts to not only find a solution, but to achieve something greater together and make a lasting impact. Welcome to my portfolio - let’s build something together!</p>
-                <div className="case-study__button-wrap">
-                    <Link to="/ux-ui" className="contactForm__button case-study__link" onClick={() => window.scrollTo(0, 0)}>See ux/ui projects!<i class="fa-solid fa-arrow-right"></i></Link>
-                    <Link to="/web-dev" className="contactForm__button case-study__link button__secondary" onClick={() => window.scrollTo(0, 0)}>See web dev projects<i class="fa-solid fa-arrow-right"></i></Link>
+        <div>
+            <div className="landing__wrapper">
+                <section className="landing__text">
+                    <h1 className="landing__title">hi, i'm <span className="landing__name--color">josie</span>!</h1>
+                    <h2 className="landing__subtitle">&gt; ux/ui <span className="landing__job-title--color">researcher</span> & <span className="landing__job-title--color">designer</span> </h2>
+                    <h2 className="landing__subtitle"> &gt; <span className="landing__job-title--color">full stack web developer</span></h2>
+                    <h2 className="landing__subtitle"> &gt; based in pennsylvania</h2>
+                    <p className="landing__blurb">I’m on a quest to combine creativity, user-friendly design, and inclusivity into adventurous digital experiences!</p> 
+                    <p className="landing__blurb">I firmly believe in collaborative efforts to not only find a solution, but to achieve something greater together and make a lasting impact. Welcome to my portfolio - let’s build something together!</p>
+                    <div className="case-study__button-wrap">
+                        <Link to="/ux-ui" className="contactForm__button case-study__link" onClick={() => window.scrollTo(0, 0)}>See ux/ui projects!<i class="fa-solid fa-arrow-right"></i></Link>
+                        <Link to="/web-dev" className="contactForm__button case-study__link button__secondary" onClick={() => window.scrollTo(0, 0)}>See web dev projects<i class="fa-solid fa-arrow-right"></i></Link>
+                    </div>
+                </section>
+                <figure className="landing__image-wrap">
+                    <img className='landing-headshot' src={headshot} alt='Headshot of Josie Griffin from her shoulders up in black and white'/>
+                </figure>
+            </div>
+            <section className="case-study__bg featured__wrapper">
+            <h2><span className="title__break">|</span> featured case studies</h2>
+                <div className="featured-projects__wrapper">
+                    {uxProjects.slice(0,2).map((uxProject, index) =>{
+                               return(
+                                   <section className="portfolio__project--card portfolio__project--card-feature" key={index}>
+                                       <div className="portfolio__project--info-feature">
+                                           <img src={uxProject.image} alt={uxProject.imgAlt} className="featured__project-image"></img>
+                                       </div>
+                                       <div
+                                       className="portfolio__project--info-feature">
+                                           <h3 className="project__title">{uxProject.name}</h3>
+                                           <ul className="project__skills--list">
+                                               {uxProject.skills.map((skill, skillIndex) => (
+                                                   <li className="label-text" key={skillIndex}>{skill}</li>
+                                               ))}
+                                           </ul>
+                                           <section>
+                                           <Link className="portfolio-case__link" to={uxProject.link} onClick={() => window.scrollTo(0, 0)}>View Case Study
+                                           </Link>
+                                           </section>
+                                          
+                                       </div>
+                                   </section>
+                               )
+                           })}
                 </div>
             </section>
-            <figure className="landing__image-wrap">
-                <img className='landing-headshot' src={headshot} alt='Headshot of Josie Griffin from her shoulders up in black and white'/>
-            </figure>
         </div>
+        
     );
 }
 
